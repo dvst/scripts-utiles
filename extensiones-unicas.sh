bff482ee56408b3f9413cc4eb67aa5eda51472fa
javierperez-axa <javier.perez@entelgy.com>
2018-09-13 18:33:34 -0500

    added a new comment
    aoeu

--- extensiones-unicas.sh
+++ extensiones-unicas.sh
@@ -4,3 +4,5 @@ find . -type f |egrep '^./.*\.' |sed -e "s/\(^.*\.\)\(.*$\)/\2/" |sort |uniq
 # un simulacro para hacer lo mismo (pero no hace lo mismo) 
 # --------------------------------------------------------
 # find . -type f |awk -F'.' '{print $NF}' |sort |uniq
+
+# new comment
